64237988386ffeb767bc90a3d92d06898fd3eef6
Armando Caleno <armandoc_81@hotmail.com>
2022-03-15 14:02:36 -0500

    borrar comentario de cabecera

--- resources/views/factura/factura.blade.php
+++ resources/views/factura/factura.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>    
     @section('body')            
-        <div class="py-12 px-6">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="py-6 px-4">
+            <div class="max-w-7xl mx-auto sm:px-4 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     
                     <form action="{{ route('factura.enviar') }}" enctype="multipart/form-data" method="post">
@@ -10,8 +10,7 @@
                         <input type="file" name="comprobante" id="comprobante" >
 
                         <button type="submit">Enviar</button>
-                    </form>
-                    
+                    </form>                    
                 </div>
             </div>
         </div>
